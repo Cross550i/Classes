b869bc9ac7405fb5242569bef3806ed13b6db03c
pavelz_fit <crause93@gmail.com>
2025-10-29 16:54:57 +0300

    - Исправил отображение brand в Truck: - Добавил класс Moto и PassengerCar

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-import typing_extensions
-
-
 class Auto:
     def __init__(self, type: str, brand: str, model: str, year: int | None = None,
                  fuel_type: str | None = None) -> None:
@@ -46,14 +43,14 @@ class Auto:
 # car_one = Auto("Легковой автомобиль", brand="BMW", model="M5", year=2015, fuel_type="Дизель")
 # car_two = Auto("Легковой автомобиль","Volkswagen", "Golf")
 # car_three = Auto("Грузовик","Volvo", "FH")
-# car_four = Auto("Мотоцик","Harley Davidson")
+# car_four = Auto("Мотоцикл","Harley Davidson")
 
 
 class Truck(Auto):
-    def __init__(self, brand: str, model: str, year: int, fuel_type: str, cargo_capacity: int) -> None:
-        super().__init__(brand, model, year, fuel_type)
+    def __init__(self, type: str, brand: str, model: str, year: int, fuel_type: str, cargo_capacity: int) -> None:
+        super().__init__(type, brand, model, year, fuel_type)
         self.cargo_capacity = cargo_capacity        # Грузоподъемность кг
-        self.current_cargo = 0  # Сколько в ней груза лежит в кг
+        self.current_cargo = 0                      # Сколько в ней груза лежит в кг
 
 
     def load_cargo(self, weight: int):
@@ -65,9 +62,26 @@ class Truck(Auto):
     def get_info(self):
         print(f"{self.brand} {self.model} Груз: {self.current_cargo}/{self.cargo_capacity} Пробег: {self.mileage}км")
 
-car_five = Truck("MAN", "FX100", 2015, "Diesel", 20000)
+# car_five = Truck("MAN", "FX100", 2015, "Diesel", 20000)
+
+class Moto(Auto):
+    def __init__(self, type: str,brand: str, model: str, year: int, weelie: bool) -> None:
+        super().__init__(type, brand, model, year)
+        self.weelie = weelie
+
+    def wellie(self):
+        if self.type == "Мотоцикл" and self.weelie == True:
+            print("Встает на дыбы")
+        else:
+            print("Не встает на дыбы")
 
 
-# car_five.load_cargo(5000)
-car_five.get_info()
 
+# car_six = Moto("Мотоцикл", "Kawasaki", "Ninja", 2023, True)
+# car_six.wellie()
+
+
+class PassengerCar(Auto):
+    def __init__(self, type: str, brand: str, model: str, year: int, fuel_type: str, max_passengers: int) -> None:
+        super().__init__(type, brand, model, year, fuel_type)
+        self.max_passengers = max_passengers
