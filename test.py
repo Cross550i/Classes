7812c7b4db30492aec3ea228293c660743661573
pavelz_fit <crause93@gmail.com>
2025-10-29 10:42:02 +0300

    Добавил type

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 class Auto:
-    def __init__(self, brand: str, model: str| None = None, year: int | None = None,
+    def __init__(self, type: str, brand: str, model: str| None = None, year: int | None = None,
                  fuel_type: str | None = None) -> None:
+        self.type = type
         self.brand = brand
         self.model = model
         self.year = year
@@ -34,15 +35,15 @@ class Auto:
 
 
     def get_info(self):
-        print(f"{self.brand} {self.model} \nГод выпуска: {self.year} \nВид топлива:"
+        print(f"{self.type} {self.brand} {self.model} \nГод выпуска: {self.year} \nВид топлива:"
               f" {self.fuel_type} \nПробег: {self.mileage}км")
 
 
 
-car_one = Auto(brand="БМВ", model="M5", year=2015, fuel_type="Дизель")
-car_two = Auto("Volkswagen", "Golf")
-car_three = Auto("Volvo", "FH")
-car_four = Auto("Harley Davidson")
+car_one = Auto("Легковой", brand="БМВ", model="M5", year=2015, fuel_type="Дизель")
+car_two = Auto("Легковой","Volkswagen", "Golf")
+car_three = Auto("Грузовик","Volvo", "FH")
+car_four = Auto("Мото","Harley Davidson")
 
 
 car_one.start_engine()
@@ -50,20 +51,20 @@ car_one.drive(5)
 car_one.get_info()
 car_one.stop_engine()
 
-
-car_two.start_engine()
-car_two.drive(10)
-car_two.get_info()
-car_two.stop_engine()
-
-car_three.start_engine()
-car_three.drive(7)
-car_three.get_info()
-car_three.stop_engine()
-
-car_four.start_engine()
-car_four.drive(12)
-car_four.get_info()
-car_four.stop_engine()
+#
+# car_two.start_engine()
+# car_two.drive(10)
+# car_two.get_info()
+# car_two.stop_engine()
+#
+# car_three.start_engine()
+# car_three.drive(7)
+# car_three.get_info()
+# car_three.stop_engine()
+#
+# car_four.start_engine()
+# car_four.drive(12)
+# car_four.get_info()
+# car_four.stop_engine()
 
 
