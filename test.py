2daf410a8c8c9d2b6727af4cb82fdf2db5538ef6
pavelz_fit <crause93@gmail.com>
2025-11-13 13:41:00 +0300

    Добавил урок по классам с смарт девайсами

--- test.py
+++ test.py
@@ -1,122 +1,47 @@
-class Auto:
-    def __init__(self, type: str, brand: str, model: str, year: int | None = None,
-                 fuel_type: str | None = None) -> None:
-        self.type = type
-        self.brand = brand
-        self.model = model
-        self.year = year
-        self.fuel_type = fuel_type
-        self.mileage = 0
-        self.is_running = False
+class SmartDevice:
+    def activate(self) -> None:
+        pass
+
+class SmartLight(SmartDevice):
+    def activate(self):
+        print("Свет включен, яркость 80%")
+        self.brightness = 80
+
+    def deactivate(self):
+        print("Свет выключен")
+
+class Thermostat(SmartDevice):
+    def activate(self):
+        print("Термостат включен (тем-ра 25')")
+        self.temperature = 25
+
+    def deactivate(self):
+        print("Термостат выключен")
 
 
-    def start_engine(self):
-        if self.is_running:
-            print(f"Двигатель {self.brand} {self.model} уже запущен")
-        else:
-            self.is_running = True
-            print(f"Двигатель {self.brand} {self.model} запустился")
+class MorningRoutine:
+    def __init__(self) -> None:
+         self.devices = []
+
+    def add_device(self, device):
+        devices.append(device)
 
 
-    def stop_engine(self):
-        if self.is_running:
-            self.is_running = False
-            print(f"Двигатель {self.brand} {self.model} остановился")
-        else:
-            print(f"Двигатель {self.brand} {self.model} уже остановлен")
+    def start_morning(self):
+        print("Запуск утреннего режима")
+        for devices in self.devices:
+            devices.activate()
+            print("Режим 'Доброе утро' активирован")
 
 
-    def drive(self, distance: int):
-        if self.is_running:
-            self.mileage += distance
-            print(f"Проехали {distance}км. Общий пробег: {self.mileage}км")
-        else:
-            print("Запустите двигатель")
-
-
-    def get_info(self):
-        print(f"{self.type} {self.brand} {self.model} \nГод выпуска: {self.year} \nВид топлива:"
-              f" {self.fuel_type} \nПробег: {self.mileage}км")
-
-
-
-# car_one = Auto("Легковой автомобиль", brand="BMW", model="M5", year=2015, fuel_type="Дизель")
-# car_two = Auto("Легковой автомобиль","Volkswagen", "Golf")
-# car_three = Auto("Грузовик","Volvo", "FH")
-# car_four = Auto("Мотоцикл","Harley Davidson")
-
-
-class Truck(Auto):
-    def __init__(self, brand: str, model: str, year: int, cargo_capacity: int) -> None:
-        super().__init__("Грузовик", brand, model, year)
-        self.cargo_capacity = cargo_capacity        # Грузоподъемность кг
-        self.current_cargo = 0                      # Сколько в ней груза лежит в кг
-
-
-    def load_cargo(self, weight: int):
-        if self.current_cargo + weight <= self.cargo_capacity:
-            all_weight = self.current_cargo + weight
-            print(f"Загружено {weight}кг., общий вес составляет {all_weight}кг.,"
-                  f"Свободно {self.cargo_capacity - all_weight}кг.")
-
-    def get_info(self):
-        print(f"{self.type} {self.brand} {self.model} Груз: {self.current_cargo}/{self.cargo_capacity} "
-              f"Пробег: {self.mileage}км")
-
-# car_five = Truck("MAN", "FX100", 2015, 20000)
-#
-# car_five.get_info()
+routine = MorningRoutine()
+routine.add_device(SmartLight)
+routine.add_device(Termostate)
+routine.start_morning()
 
 
 
 
 
-class Moto(Auto):
-    def __init__(self, brand: str, model: str, year: int, weelie: bool) -> None:
-        super().__init__("Мотоцикл", brand, model, year)
-        self.weelie = weelie
-
-    def wellie(self):
-        if self.weelie:
-            print("Встает на дыбы")
-        else:
-            print("Не встает на дыбы")
-
-
-    def get_info(self):
-        print(f"{self.type} {self.brand} {self.model} {self.year}")
-
-
-
-# car_six = Moto("Kawasaki", "Ninja", 2023, False)
-# car_six.get_info()
-
-
-class PassengerCar(Auto):
-    def __init__(self, brand: str, model: str, year: int, fuel_type: str, max_passengers: int)\
-            -> None:
-        super().__init__("Маршрутка", brand, model, year, fuel_type)
-        self.max_passengers = max_passengers
-
-
-class Electro(Auto):
-    def __init__(self, brand: str, model: str, year: int, charge: int) -> None:
-        super().__init__("Электро", brand, model, year)
-        self.charge = charge
-
-
-    def start_engine(self):
-        if self.charge == 0:
-            print(f"Аккумулятор {self.brand} {self.model} разряжен")
-        else:
-            print(f"Двигатель {self.brand} {self.model} запустился")
-
-
-    def get_info(self):
-        print(f"{self.type} {self.brand} {self.model} {self.year} Заряд аккумулятора-{self.charge}%")
-
-car_seven = Electro("Tesla", "Model 3", 2024, 50)
-car_seven.get_info()
-car_seven.start_engine()
 
 
